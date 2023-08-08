e8458be40210e9d5ebafec93bbff4b7b3ec7e3fd
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-08-08 13:42:10 +0900

    Rtan

---
+++ FirstWeekProject/Assets/Scripts/gameManager.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class gameManager : MonoBehaviour
+{
+    public Text timeTxt;
+    public GameObject card;
+    float time = 0.0f;
+    public static gameManager I;
+    public GameObject firstCard;
+    public GameObject secondCard;
+    public GameObject endTxt;
+
+    public AudioClip match;
+    public AudioSource audioSource;
+
+
+    void Awake()
+    {
+        I = this;
+    }
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        int[] rtans = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+
+        rtans = rtans.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
+
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject newCard = Instantiate(card);
+            newCard.transform.parent = GameObject.Find("cards").transform;
+
+            float x = (i / 4) * 1.4f - 2.1f;
+            float y = (i % 4) * 1.4f - 3.0f;
+            newCard.transform.position = new Vector3(x, y, 0);
+
+            string rtanName = "rtan" + rtans[i].ToString();
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(rtanName);
+        }
+    }
+        // Update is called once per frame
+        void Update()
+        {
+            time += Time.deltaTime;
+            timeTxt.text = time.ToString("N2");
+
+             if(time >= 30)
+                 {
+                     GameEnd();
+
+                 }
+        }
+
+    public void isMatched()
+    {
+        string firstCardImage = firstCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name;
+        string secondCardImage = secondCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name;
+
+        if (firstCardImage == secondCardImage)
+        {
+
+            audioSource.PlayOneShot(match);
+            
+            firstCard.GetComponent<card>().destroyCard();
+            secondCard.GetComponent<card>().destroyCard();
+
+            int cardsLeft = GameObject.Find("cards").transform.childCount;
+            if (cardsLeft == 2)
+            {
+                endTxt.SetActive(true);
+                Time.timeScale = 0.0f;
+                Invoke("GameEnd", 1f);
+            }
+        }
+        else
+        {
+            Debug.Log("같지 않다!");
+            firstCard.GetComponent<card>().closeCard();
+            secondCard.GetComponent<card>().closeCard();
+        }
+
+        firstCard = null;
+        secondCard = null;
+    }
+
+    void GameEnd()
+    {
+        Time.timeScale = 0f;
+        endTxt.SetActive(true);
+    }
+
+}
+
