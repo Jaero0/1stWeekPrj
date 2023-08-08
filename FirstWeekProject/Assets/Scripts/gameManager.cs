0c9141e8b0cd9266c3cd1eaee45e6893568fedbf
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-08-08 20:26:44 +0900

    송승훈 담당 기능

--- FirstWeekProject/Assets/Scripts/gameManager.cs
+++ FirstWeekProject/Assets/Scripts/gameManager.cs
@@ -8,15 +8,23 @@ using System.Linq;
 public class gameManager : MonoBehaviour
 {
     public Text timeTxt;
+
+    public Text teamName;
     public GameObject card;
     float time = 0.0f;
+    int count = 0;//:ssh
     public static gameManager I;
     public GameObject firstCard;
     public GameObject secondCard;
-    public GameObject endTxt;
+    public GameObject endpenal;
+    
+    public Text scoreTxt;//matching score :ssh
 
-    public AudioClip match;
+    string humanName;
+    public AudioClip match; 
     public AudioSource audioSource;
+    
+
 
 
     void Awake()
@@ -25,22 +33,25 @@ public class gameManager : MonoBehaviour
     }
     void Start()
     {
+
+
+
         Time.timeScale = 1.0f;
-        int[] rtans = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        int[] rtans = { 0,0,1,1,2,2,3,3,4,4,5,5};
 
         rtans = rtans.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
 
-        for (int i = 0; i < 16; i++)
+        for (int i = 0; i < 12; i++)
         {
             GameObject newCard = Instantiate(card);
             newCard.transform.parent = GameObject.Find("cards").transform;
 
-            float x = (i / 4) * 1.4f - 2.1f;
+            float x = (i / 4) * 1.4f - 1.4f;
             float y = (i % 4) * 1.4f - 3.0f;
             newCard.transform.position = new Vector3(x, y, 0);
 
-            string rtanName = "rtan" + rtans[i].ToString();
-            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(rtanName);
+            humanName = "human" + rtans[i].ToString();
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(humanName);
         }
     }
         // Update is called once per frame
@@ -54,7 +65,8 @@ public class gameManager : MonoBehaviour
                      GameEnd();
 
                  }
-        }
+        scoreTxt.text = "매칭횟수: " + count.ToString();
+    }
 
     public void isMatched()
     {
@@ -63,35 +75,57 @@ public class gameManager : MonoBehaviour
 
         if (firstCardImage == secondCardImage)
         {
-
+            if (firstCardImage == "human0" || firstCardImage == "human1")
+            {
+                teamName.text = "정재호";
+            }
+            else if (firstCardImage == "human2" || firstCardImage == "human3")
+            {
+                teamName.text = "김준범";
+            }
+            else if (firstCardImage == "human4" || firstCardImage == "human5")
+            {
+                teamName.text = "송승훈";
+            }
+           
             audioSource.PlayOneShot(match);
-            
+
             firstCard.GetComponent<card>().destroyCard();
             secondCard.GetComponent<card>().destroyCard();
 
+            count++; //matching score :ssh
             int cardsLeft = GameObject.Find("cards").transform.childCount;
             if (cardsLeft == 2)
             {
-                endTxt.SetActive(true);
-                Time.timeScale = 0.0f;
-                Invoke("GameEnd", 1f);
-            }
-        }
-        else
-        {
-            Debug.Log("같지 않다!");
-            firstCard.GetComponent<card>().closeCard();
-            secondCard.GetComponent<card>().closeCard();
-        }
 
-        firstCard = null;
-        secondCard = null;
+                GameEnd();
+            }
+           
+        }
     }
 
-    void GameEnd()
+    public void deMatched()  // 나눈 이유 메치가 안됬을때 일어날수있는 이벤트가잇을수도있어서  
     {
+        string firstCardImage = firstCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name;
+        string secondCardImage = secondCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name;
+        if (firstCardImage != secondCardImage)
+        {
+            count++; //matching score: ssh
+            firstCard.GetComponent<card>().closeCard();
+            secondCard.GetComponent<card>().closeCard();
+            teamName.text = "실패 ㅠ";
+        }
+        firstCard = null;
+        secondCard = null;
+
+    }
+
+
+
+        void GameEnd()
+    {
+        endpenal.SetActive(true);
         Time.timeScale = 0f;
-        endTxt.SetActive(true);
     }
 
 }
